4f8e115a0868f6b653ee8ede7d2ac37cad20e5d4
Davenury <dawid199960@gmail.com>
2022-06-01 17:13:37 +0200

    full working

--- containernet.py
+++ containernet.py
@@ -4,30 +4,33 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import info, setLogLevel
 import json
+import sys
 
 
 setLogLevel('info')
 
-
 SINGLE_INSTANCE = "single_instance"
 INSTANCES_KEY = "instances"
 BENCHMARK = "benchmark"
-CLIENTS = "clients"
 THREADS = "threads"
 REQUESTS = "requests"
 RATIO = "ratio"
 CPUS = "cpus"
 
+
+net = Containernet(controller=Controller)
+info('Adding controller\n')
+net.addController(f'c0')
+
 def get_benchmark_values(config):
     benchmark_values = config.get(BENCHMARK, None)
     if benchmark_values is None:
-        return 5, 4, 10000, "1:10"
+        return 4, 10000, "1:10"
     
-    clients = config.get(CLIENTS, 5)
-    threads = config.get(THREADS, 4)
-    requests = config.get(REQUESTS, 10000)
-    ratio = config.get(RATIO, "1:10")
-    return clients, threads, requests, ratio
+    threads = benchmark_values.get(THREADS, 4)
+    requests = benchmark_values.get(REQUESTS, 10000)
+    ratio = benchmark_values.get(RATIO, "1:10")
+    return threads, requests, ratio
     
 
 def read_config():
@@ -46,12 +49,10 @@ config = read_config()
 redis_hosts = get_redis_hosts(config)
 
 def make_one_cpu(cpu, config):
-    net = Containernet(controller=Controller)
-    info('Adding controller\n')
-    net.addController('c0')
+
     info('Adding redis\n')
 
-    s1 = net.addSwitch('s1')
+    s1 = net.addSwitch(f's1')
 
     for idx, host in enumerate(redis_hosts):
         redis_server = net.addDocker(f'redis{idx}', ip=host, dimage='wrapped_redis:latest', ports=[6379], dcmd="redis-server", cpu_quota=cpu*1000)
@@ -60,19 +61,20 @@ def make_one_cpu(cpu, config):
 
     client = net.addDocker('client', ip="10.0.0.50", dimage='wrapped_benchmark:latest', network_mode="host")
 
-    clients, threads, requests, ratio = get_benchmark_values(config)
+    threads, requests, ratio = get_benchmark_values(config)
 
     net.addLink(s1, client)
 
     net.start()
 
-    info(client.cmd(f"memtier_benchmark -s {redis_hosts[0]} -c {clients} -t {threads} -n {requests} --ratio {ratio} --hide-histogram --json-out-file=result{cpu}.json"))
+    net.ping([redis_server, client])
+
+    info(client.cmd(f"memtier_benchmark -s {redis_hosts[0]} -c 1 -t {threads} -n {requests} --ratio {ratio} --hide-histogram --json-out-file=result{cpu}.json"))
     client.cmd(f'cat result{cpu}.json | curl -H "Content-Type: application/json" -X POST -d "$(</dev/stdin)" http://localhost:8080/{cpu}')
 
     net.stop()
 
 cpus = config.get(CPUS, [10, 50, 100])
 
-for cpu in cpus:
-    info(f"Iterating for {cpu}% cpu")
-    make_one_cpu(cpu, config)
+
+make_one_cpu(int(sys.argv[1]), config)
--- script.sh
+++ script.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-sudo docker run --name containernet -d --rm -it --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock containernet/containernet
+sudo apt-get install -y jq
 
 sudo docker build -f server/Dockerfile -t node_app server/
 
 sudo docker build -f Dockerfile.redis -t wrapped_redis .
 sudo docker build -f Dockerfile.benchmark -t wrapped_benchmark .
 
-DOCKER_CONTAINER_ID=`sudo docker ps | grep containernet | awk '{print $1}'`
+sudo docker run --rm -d -p 8080:8080 node_app
 
-sudo docker run -d -p 8080:8080 node_app
+for cpu in $(cat config.json | jq '.cpus' | jq '.[]' -c); do
 
-sudo docker cp containernet.py $DOCKER_CONTAINER_ID:/containernet
-sudo docker cp config.json $DOCKER_CONTAINER_ID:/containernet
+    sudo docker run --name containernet -d --rm -it --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock containernet/containernet
 
-sudo docker exec -it $DOCKER_CONTAINER_ID sudo python3 containernet.py
+    CONTAINERNET_CONTAINER_ID=`sudo docker ps | grep containernet | awk '{print $1}'`
 
-#sudo docker cp node_app:/app/result.csv .
+    sudo docker cp containernet.py $CONTAINERNET_CONTAINER_ID:/containernet
+    sudo docker cp config.json $CONTAINERNET_CONTAINER_ID:/containernet
+
+    sudo docker exec -t $CONTAINERNET_CONTAINER_ID sudo python3 containernet.py $cpu
+
+    sudo docker kill $CONTAINERNET_CONTAINER_ID
+
+done;
+
+NODE_DOCKER_CONTAINER=`sudo docker ps | grep node_app | awk '{print $1}'`
+REDIS_CONTAINER_ID=`sudo docker ps | grep redis-server | awk '{print $1}'`
+
+sudo docker cp $NODE_DOCKER_CONTAINER:/app/result.csv .
+
+sudo docker kill `sudo docker ps -q`
+
+sudo docker rm $REDIS_CONTAINER_ID
